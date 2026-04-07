9fcefe3a74a751b34aee1cb5bc9feecaa4430a65
Dung246 <dungoo2605@gmail.com>
2026-04-07 19:52:47 +0700

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,19 @@
 package com.example;
 
+import com.example.config.AppConfig;
+import com.example.model.SystemConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        SystemConfig config = context.getBean(SystemConfig.class);
+
+        System.out.println("Chi nhánh: " + config.getBranchName());
+        System.out.println("Giờ mở cửa: " + config.getOpeningHour());
+    }
 }
